a3a8dcb63947323f29eb95a5220da488b3759bd0
Brian Chew <brian@bchewy.com>
2018-11-20 16:29:30 +0800

    Added person.cs

--- WebRole1/Models/Person.cs
+++ WebRole1/Models/Person.cs
@@ -7,5 +7,10 @@ namespace WebRole1.Models
 {
     public class Person
     {
+        public int PersonID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }    
+        public char Gender { get; set; }
+        public string Citizenship { get; set; } 
     }
 }
