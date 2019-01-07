1c67cece952f146e369bccb9474c6f3c2a6cf6b6
Kai Woon Low <s10182100@connect.np.edu.sg>
2019-01-07 17:00:26 +0800

    Add project files.

---
+++ WebRole1/Models/Patient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRole1.Models
+{
+    public class Patient : Person
+    {
+        public int PatientID { get; set; }
+        public string Address { get; set; }
+        public string ICNo { get; set; }
+    }
+}
