6e1db010c0a2541ba0d2820c2b25f3d0b3d09e97
Kai Woon Low <s10182100@connect.np.edu.sg>
2019-01-07 16:50:49 +0800

    Add project files.

---
+++ WebRole1/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebRole1.Models
+{
+    public abstract class Person
+    {
+        public int PersonID { get; set; }
+
+        [ForeignKey("VideoHistory")]
+        public int VideoID { get; set; }
+
+        public string Name { get; set; }
+        public int Age { get; set; }    
+        public char Gender { get; set; }
+        public string Citizenship { get; set; } 
+
+        public virtual ICollection<VideoHistory> VideoHistories { get; set; }
+    }
+}
