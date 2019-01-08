7e7b94a7c62556d25dbcb76ca569ab7c398f22d4
Hsin Kai <s10179111@connect.np.edu.sg>
2019-01-08 10:52:50 +0800

    Add project files.

---
+++ WebRole1/Models/Doctor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebRole1.Models
+{
+    public class Doctor : Person
+    {
+        
+        public int DoctorID { get; set; }
+        public string Specialty { get; set; }
+        public int FacultyID { get; set; }
+    }
+}
