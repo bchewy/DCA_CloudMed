c0e3a5b7dcc9e7882ce827f62de3a8838723594c
Brian Chew <brianchew32@hotmail.com>
2019-01-20 19:19:38 +0800

    Scaffold Consultation Controllers and Views

---
+++ WebRole1/Controllers/ConsultationsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebRole1.DAL;
+using WebRole1.Models;
+
+namespace WebRole1.Controllers
+{
+    public class ConsultationsController : Controller
+    {
+        private CloudMedContext db = new CloudMedContext();
+
+        // GET: Consultations
+        public ActionResult Index()
+        {
+            var consultations = db.Consultations.Include(c => c.Patient);
+            return View(consultations.ToList());
+        }
+
+        // GET: Consultations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consultation consultation = db.Consultations.Find(id);
+            if (consultation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consultation);
+        }
+
+        // GET: Consultations/Create
+        public ActionResult Create()
+        {
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "Address");
+            return View();
+        }
+
+        // POST: Consultations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ConsultationID,QueueNo,TimeStamp,Status,ConsultationType,PatientID")] Consultation consultation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Consultations.Add(consultation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "Address", consultation.PatientID);
+            return View(consultation);
+        }
+
+        // GET: Consultations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consultation consultation = db.Consultations.Find(id);
+            if (consultation == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "Address", consultation.PatientID);
+            return View(consultation);
+        }
+
+        // POST: Consultations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ConsultationID,QueueNo,TimeStamp,Status,ConsultationType,PatientID")] Consultation consultation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(consultation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PatientID = new SelectList(db.Patients, "PatientID", "Address", consultation.PatientID);
+            return View(consultation);
+        }
+
+        // GET: Consultations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consultation consultation = db.Consultations.Find(id);
+            if (consultation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consultation);
+        }
+
+        // POST: Consultations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Consultation consultation = db.Consultations.Find(id);
+            db.Consultations.Remove(consultation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
