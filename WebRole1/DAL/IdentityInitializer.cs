f68c98e4469064fd3c2dbe635ee3c668088cd046
Brian Chew <brianchew32@hotmail.com>
2019-01-22 19:39:28 +0800

    Modified patient.cs Added migration files for patient.cs modification.

---
+++ WebRole1/DAL/IdentityInitializer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using WebRole1.Models;
+
+namespace WebRole1.DAL
+{
+    public class IdentityInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            InitializeIdentityForEF(context);
+            base.Seed(context);
+        }
+        public static void InitializeIdentityForEF(ApplicationDbContext db)
+        {
+            var userManager = HttpContext.Current.GetOwinContext()
+            .GetUserManager<ApplicationUserManager>();
+            var roleManager = HttpContext.Current.GetOwinContext()
+            .Get<ApplicationRoleManager>();
+            //Define a user account and its role
+            string name = "admin@admin.com";
+            string password = "adminPass";
+            string roleName = "Administrator";
+            //Create Administrator role if it does not exist
+            var role = roleManager.FindByName(roleName);
+            if (role == null)
+            {
+                role = new IdentityRole(roleName);
+                var roleresult = roleManager.Create(role);
+            }
+            //Create the user account if it does not exist
+            var user = userManager.FindByName(name);
+            if (user == null)
+            {
+                user = new ApplicationUser { UserName = name, Email = name };
+                var result = userManager.Create(user, password);
+                result = userManager.SetLockoutEnabled(user.Id, false);
+            }
+            //Assign the new user account with Administrator role
+            var rolesForUser = userManager.GetRoles(user.Id);
+            if (!rolesForUser.Contains(role.Name))
+            {
+                var result = userManager.AddToRole(user.Id, role.Name);
+            }
+        }
+    }
+}
