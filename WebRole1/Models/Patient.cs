064ed5d527da1e48ecee76ee22eab2ba3c5bea31
Brian Chew <brianchew32@hotmail.com>
2019-01-22 19:39:28 +0800

    Modified patient.cs Added migration files for patient.cs modification.

--- WebRole1/Models/Patient.cs
+++ WebRole1/Models/Patient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -14,6 +15,12 @@ namespace WebRole1.Models
 
         //[ForeignKey("Consultation")]
         //public int ConsultationID { get; set; }
+
+        public string PatientImageURL { get; set; }
+
+        [DisplayName("Thumbnail")]
+        public string PatientThumbNailURl { get; set; }
+
 
         public string Address { get; set; }
         public DateTime DoB { get; set; }
