717879cbb0eb673afbaf94d0f26a0bdae51f38bd
Kai Woon Low <s10182100@connect.np.edu.sg>
2019-01-26 16:37:48 +0800

    MR done w vm

---
+++ WebRole1/Models/MedicalRecordsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+namespace WebRole1.Models
+{
+    public class MedicalRecordsViewModel
+    {
+        [Key]
+        public int RecordID { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string DocURL { get; set; }
+
+        public string Illness { get; set; }
+
+        [ForeignKey("Consultation")]
+        public int ConsultationID { get; set; }
+
+    }
+}
